76ced37af64ec428d3462ec09b624a95bd9d8297
Nithin Mathew <nithin.mathew@sysfore.com>
2024-02-22 14:20:27 +0530

    Dotnet 6 CRUD Application

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ASPDotNetCRUDApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPDotNetCRUDApp.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public DbSet<Team> Teams { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options) 
+        {
+            
+        }
+    }
+}
